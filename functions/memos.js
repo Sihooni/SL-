0491e055bc2e83bbd9dd80e38d7c99bb7599a9d4
Sihooni <sh010410@gmail.com>
2023-05-24 20:22:26 +0900

    Update memos.js

--- functions/memos.js
+++ functions/memos.js
@@ -1,11 +1,29 @@
 // memos.js
 
+const fs = require('fs');
+const path = require('path');
+
+const memosFilePath = path.join(__dirname, 'memos.json');
+
 let memos = [];
+
+// Load existing memos from file
+try {
+  const memosData = fs.readFileSync(memosFilePath, 'utf-8');
+  memos = JSON.parse(memosData);
+} catch (error) {
+  console.log('Error reading memos file:', error);
+}
+
+const saveMemosToFile = () => {
+  fs.writeFileSync(memosFilePath, JSON.stringify(memos), 'utf-8');
+};
 
 exports.handler = async (event) => {
   if (event.httpMethod === 'POST') {
     const { memo } = JSON.parse(event.body);
     memos.push(memo);
+    saveMemosToFile();
     return {
       statusCode: 201,
       body: JSON.stringify({ message: 'Memo stored successfully' })
